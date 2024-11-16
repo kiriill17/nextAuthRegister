e4ac59740e8096574833613e7bdffb269258bd8b
Kirill Zakharov <kiriill17@icloud.com>
2024-11-16 18:44:23 +0500

    comment

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,13 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       {children}
+      <Analytics />
       <Toaster />
     </SessionProvider>
   );
